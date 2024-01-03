4dc6d1eefab739310470ffb91e1d9dad1e85a37b
Tim Gesemann <tim.gesemann@volkswagen.de>
2024-01-03 14:42:08 +0100

    ✨ (schedule): alter schedule so that tweets are only fired twice a day @ 10 an 14

--- src/lib/stacks/serverless-twitter-bot-stack.ts
+++ src/lib/stacks/serverless-twitter-bot-stack.ts
@@ -25,7 +25,7 @@ export class ServerlessTwitterBotStack extends cdk.Stack {
         },
       },
       ruleProps: {
-        schedule: Schedule.expression('cron(0 8-17 * * ? *)'),
+        schedule: Schedule.expression('cron(0 10,14 * * ? *)'),
       },
     });
   }
